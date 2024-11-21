f0b2dc46c12a270cc396cff5c493d0a5370508e6
songhyeongju <hyeongah9@gmail.com>
2024-11-21 20:20:15 +0900

    ternary

---
+++ ternary.js
@@ -0,0 +1,9 @@
+// 삼항 연산 표현 방법
+let favorite = 'apple';
+let pick = favorite == 'apple' ? 'yes' : 'no';
+
+console.log(pick); // yes
+
+let num = 3;
+let whatsNum = num / 2 == 0 ? 'even number' : 'odd number';
+console.log(whatsNum); //odd number
