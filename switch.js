c76a4d2e71c043de1f70b7547a7f259fc6a12541
songhyeongju <hyeongah9@gmail.com>
2024-11-21 20:32:17 +0900

    switch

---
+++ switch.js
@@ -0,0 +1,28 @@
+// switch는 꼭 break를 포함 시켜줘야한다
+let fruit = 0;
+let fruitName;
+switch (fruit) {
+  case 0:
+    fruitName = 'apple';
+    break;
+  case 1:
+    fruitName = 'banana';
+    break;
+  case 2:
+    fruitName = 'orange';
+    break;
+}
+console.log(fruitName); // apple
+
+let fruits = 'banana';
+let favorite;
+switch (fruits) {
+  case 'apple':
+  case 'banana':
+    favorite = 'yes';
+    break;
+  case 'orange':
+    favorite = 'no';
+    break;
+}
+console.log(favorite); // yes
